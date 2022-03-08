859bd9d552ef5aff5f1d7968c1461557ad2b37c8
Michael Hicks <hicks.michael04@gmail.com>
2022-03-08 10:36:45 -0700

    Add instance methods to hospital model

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -1,3 +1,11 @@
 class Hospital < ApplicationRecord
   has_many :doctors
+
+  def doctor_count
+    doctors.count
+  end
+
+  def university_unique_list
+    doctors.distinct(:university).pluck(:university)
+  end
 end
