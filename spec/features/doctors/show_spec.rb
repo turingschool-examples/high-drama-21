db7e3735100054ea77f461fee772e669ceb22f5e
Tamara Dowis <tamara.dowis@gmail.com>
2022-03-08 11:09:46 -0600

    -mtest doctor hospital name and all patients

--- spec/features/doctors/show_spec.rb
+++ spec/features/doctors/show_spec.rb
@@ -7,20 +7,17 @@ RSpec.describe 'Doctor Show page', type: :feature do
    it 'doctor name specialty, uni, hospital and patients' do
 
      hospital1 = Hospital.create!(name: "Grey Sloan Memorial Hospital")
-     hospita2l = Hospital.create!(name: "Mike Dani Memorial Hospital")
+     hospital2 = Hospital.create!(name: "Mike Dani Memorial Hospital")
      doctor1 = hospital1.doctors.create!(name: "Miranda Bailey", specialty: "General Surgery", university: "Stanford University")
-     doctor2 = hospital1.doctors.create!(name: "Bob Lemon", specialty: "Heart", university: "NYU")
-     doctor3 = hospital2.doctors.create!(name: "Sarah Lime", specialty: "Lung", university: "Ga Tech")
-     doctor4 = hospital2.doctors.create!(name: "Tenzin Davidy", specialty: "Foot", university: "CDMX Univ")
-     doctor5 = hospital2.doctors.create!(name: "Cathy Marie", specialty: "Brain", university: "Cali University")
+
      patient1 = Patient.create!(name: 'Red', age: 32)
      patient2 = Patient.create!(name: 'Apple', age: 17 )
      patient3 = Patient.create!(name: 'Green', age: 29)
      patient4 = Patient.create!(name: 'Blue', age: 87)
      patient5 = Patient.create!(name: 'Orange', age: 4)
      patient6 = Patient.create!(name: 'Yellow', age: 27)
-     DoctorPatient.create!(doctor_id: doctor1.id., patient_id: patient1.id)
-     DoctorPatient.create!(doctor_id: doctor1.id., patient_id: patient2.id)
+     DoctorPatient.create!(doctor_id: doctor1.id, patient_id: patient1.id)
+     DoctorPatient.create!(doctor_id: doctor1.id, patient_id: patient2.id)
      # DoctorPatient.create!(doctor_id: doctor2.id., patient_id: patient3.id)
      # DoctorPatient.create!(doctor_id: doctor2.id., patient_id: patient4.id)
      # DoctorPatient.create!(doctor_id: doctor3.id., patient_id: patient5.id)
@@ -33,7 +30,6 @@ RSpec.describe 'Doctor Show page', type: :feature do
    expect(page).to have_content(doctor1.name)
    expect(page).to have_content(doctor1.specialty)
    expect(page).to have_content(doctor1.university)
-   expect(page).to have_content(doctor1.hospital_name)
    expect(page).to have_content(doctor1.hospital_name)
 
    expect(page).to have_content(patient1.name)
