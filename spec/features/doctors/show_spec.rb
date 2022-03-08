f261504c8c1ad600e086065b068f9b0e04133229
Tamara Dowis <tamara.dowis@gmail.com>
2022-03-08 11:18:03 -0600

    -mtest: US 2

--- spec/features/doctors/show_spec.rb
+++ spec/features/doctors/show_spec.rb
@@ -13,18 +13,10 @@ RSpec.describe 'Doctor Show page', type: :feature do
      patient1 = Patient.create!(name: 'Red', age: 32)
      patient2 = Patient.create!(name: 'Apple', age: 17 )
      patient3 = Patient.create!(name: 'Green', age: 29)
-     patient4 = Patient.create!(name: 'Blue', age: 87)
-     patient5 = Patient.create!(name: 'Orange', age: 4)
-     patient6 = Patient.create!(name: 'Yellow', age: 27)
+
      DoctorPatient.create!(doctor_id: doctor1.id, patient_id: patient1.id)
      DoctorPatient.create!(doctor_id: doctor1.id, patient_id: patient2.id)
-     # DoctorPatient.create!(doctor_id: doctor2.id., patient_id: patient3.id)
-     # DoctorPatient.create!(doctor_id: doctor2.id., patient_id: patient4.id)
-     # DoctorPatient.create!(doctor_id: doctor3.id., patient_id: patient5.id)
-     # DoctorPatient.create!(doctor_id: doctor3.id., patient_id: patient6.id)
-     # DoctorPatient.create!(doctor_id: doctor4.id., patient_id: patient4.id)
-     # DoctorPatient.create!(doctor_id: doctor4.id., patient_id: patient2.id)
-     # DoctorPatient.create!(doctor_id: doctor5.id., patient_id: patient1.id)
+
     visit doctor_path(doctor1)
 
    expect(page).to have_content(doctor1.name)
@@ -35,7 +27,6 @@ RSpec.describe 'Doctor Show page', type: :feature do
    expect(page).to have_content(patient1.name)
    expect(page).to have_content(patient2.name)
    expect(page).to_not have_content(patient3.name)
-
 
    end
   end
