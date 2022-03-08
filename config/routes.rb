19d75b6781bb71a0e1c09fdc20960f3231bd2539
Michael Hicks <hicks.michael04@gmail.com>
2022-03-08 11:08:13 -0700

    Create destroy routes to DoctorPatientsController CORRECTLY

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+
   resources :doctors, only: [:show]
   resources :hospitals, only: [:show]
+  delete '/doctors/:doctor_id/patients/:patient_id', to: 'doctor_patients#destroy'
 
 end
